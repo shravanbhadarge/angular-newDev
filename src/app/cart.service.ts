e6831770f3c550249cf398e939875a0a96ce9dab
shravanbhadarge <114379926+shravanbhadarge@users.noreply.github.com>
2022-09-28 13:39:27 +0530

    2 files modified, 2 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Product } from './products';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
   items: Product[] = [];
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  getShippingPrices() {
+    return this.http.get<{ type: string; price: number }[]>(
+      '/assets/shipping.json'
+    );
+  }
 
   addToCart(product: Product) {
     this.items.push(product);
